eaa74bc8ca20dc29845774093aac5cfb80baaccc
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-07-16 10:18:54 +0000

    Tagged version 0.1

---
+++ chrome/extensions/hoverzoom/tags/0.1/plugins/default.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2010 Romain Vallet
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	"name": "Default",
+	"version": "0.1",
+	"prepareImgLinks": function() {
+		var links = $("a[href]").filter(function(index) {
+			return this.href.match(/^[^\?]*\.(jpg|jpeg|gif|png)$/i);
+		});
+		links.each(function() {
+			$(this).data('hoverzoomsrc', this.href);
+		});
+		return links;	
+	}
+});
