8c6f648dc4200471f4c2c378d41c2973ab6058f0
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-10-12 16:17:54 +0000

    Tagged version 1.4

---
+++ src/tags/1.4/plugins/lastfm.js
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2010 Romain Vallet
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'Last.fm',
+	version: '0.1',
+	prepareImgLinks: function(callback) {
+		var res = [];
+		hoverZoom.urlReplace(res, 
+			'img[src*=/serve/]',
+			/\/serve\/.*\//,
+			'/serve/_/',
+			':not(.image, .art):eq(0)'
+		);			
+		/*
+		// Videos
+		hoverZoom.urlReplace(res, 
+			'img[src*=img.youtube.com], img[src*=i.ytimg.com]',
+			/[1-9]+\.jpg/,
+			'0.jpg'
+		);*/			
+		callback($(res));	
+	}
+});
