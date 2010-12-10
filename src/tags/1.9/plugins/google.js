967a9a66449e5475374ee103c01218edb7d078a5
romain.vallet <romain.vallet@439940d7-989b-cf84-2b3c-aac36f038e56>
2010-12-10 16:15:03 +0000

    Tagged version 1.9

---
+++ src/tags/1.9/plugins/google.js
@@ -0,0 +1,63 @@
+// Copyright (c) 2010 Romain Vallet
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'Google',
+	version: '0.6',
+	prepareImgLinks: function(callback) {
+	
+		function getSrc(link) {
+			var href = link.attr('href');
+			if (!href) { return null; }
+			var imgUrlIndex = href.indexOf('imgurl=');
+			if (imgUrlIndex > -1) {
+				return decodeURIComponent(href.substring(imgUrlIndex + 7, href.indexOf('&', imgUrlIndex)));		
+			} else {
+				return null;
+			}
+		}
+		
+		var res = [];
+		$('#iur a[href], #ImgCont a[href]').each(function() {
+			var _this = $(this);
+			if (_this.attr('href')) {
+				var src = getSrc(_this);
+				if (src) {
+					_this.data('hoverZoomSrc', [src]);
+					res.push(_this);
+				}
+			}
+		});		
+		callback($(res));
+		
+		
+		
+		$('#rg_hta').mousemove(function() {
+			var _this = $(this);
+			if (_this.data('hoverZoomSrc')) { return; }
+			var src = getSrc(_this);
+			if (src) {
+				_this.addClass('hoverZoomLink').data('hoverZoomSrc', [src]);
+			}
+		}).mouseleave(function() {
+			$(this).removeData('hoverZoomSrc');
+		});
+		
+		/*$('.rg_l').mousemove(prepareRgHl);
+		$(window).bind('DOMNodeInserted', function(event) {
+			var el = event.srcElement
+			if (el && el.getAttribute && el.getAttribute('id') == 'rg_h') {
+				setTimeout(function() {
+					prepareRgHl();
+					console.log('appel de mousemove');
+					$(document).trigger('mousemove');
+					//$(document).mousemove();
+					//var evt = document.createEvent("MouseEvents");
+					//evt.initEvent("mousemove", true, true);
+					//document.getElementById('rg_hl').dispatchEvent(evt);
+				}, 1000);
+			}
+		});*/
+	}
+});
---
+++ src/tags/1.9/plugins/imgur_a.js
@@ -0,0 +1,63 @@
+﻿// Copyright (c) 2010 Romain Vallet
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'Imgur',
+	version: '0.2',
+	prepareImgLinks: function(callback) {
+	
+		var res = [],
+			minSplitLength = 4;
+		
+		function prepareImgLink() {
+			var link = $(this);
+			if (link.data('hoverZoomSrc')) { return; }
+			var aHref = link.attr('href').split('/');
+			if (aHref.length < minSplitLength) { return; }
+			var excl = ['delete', 'forum', 'removalrequest', 'contact', 'upgrade', 'tools', 'stats', 'logout', 'signin', 'register', 'blog'];
+			if (excl.indexOf(aHref[minSplitLength - 1]) > -1) { return; }
+			var hash = '';
+			try {
+				while (!hash) {
+					hash = aHref.pop().trim();
+				}
+			} catch(e) {}
+			if (aHref.length < minSplitLength - 1 || hash.length < 5) { return; }
+			var i = hash.indexOf('?');
+			if (i > -1) {
+				hash = hash.substr(0, i);
+			}
+			i = hash.indexOf('&');
+			if (i > -1) {
+				hash = hash.substr(0, i);
+			}
+			excl.push('gallery');
+			if (excl.indexOf(hash) > -1) { return; }
+			i = hash.indexOf('/');
+			if (i > -1) {
+				hash = hash.substr(0, i);
+			}
+			var srcs;
+			if (hash.indexOf('.') == -1) {
+				var url = 'http://i.imgur.com/' + hash;
+				srcs = [url + '.jpg', url + '.png', url + '.gif'];
+			} else {
+				srcs = ['http://i.imgur.com/' + hash];
+			}
+			link.data('hoverZoomSrc', srcs);
+			res.push(link);
+		}
+	
+		// Every sites
+		$('a[href*=//imgur.com/], a[href*=//i.imgur.com/]').each(prepareImgLink);
+		
+		// On imgur.com (galleries, etc)
+		if (window.location.host.indexOf('imgur.com') > -1) {
+			minSplitLength = 2;
+			$('a[href^=/]').each(prepareImgLink);			
+		}
+		
+		if (res.length) { callback($(res));	}
+	}
+});
