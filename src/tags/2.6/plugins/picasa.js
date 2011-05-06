44d03fadd482566a8e58c83c0fa8f80ccaae77d5
romain.vallet@gmail.com <romain.vallet@gmail.com@439940d7-989b-cf84-2b3c-aac36f038e56>
2011-05-06 13:53:35 +0000

    Tagged version 2.6

---
+++ src/tags/2.6/plugins/picasa.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2010 Romain Vallet
+// Licensed under the MIT license, read license.txt
+
+// The dynamic loading of images in Picasa Web Albums makes it difficult to handle with this extension.
+// The function that "prepares" a link has been isolated (prepareImgLink) and is called whenever the
+// user move the mouse over a link.
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+	name: 'Picasa Web Albums',
+	version: '0.5',
+	prepareImgLinks: function(callback) {
+	
+		function prepareImgLink() {
+			var _this = $(this);
+			if (_this.data('hoverZoomSrc')) { return; }
+			var src = _this.find('img')[0].src;
+			src = src.replace(/\/s\d+(-c)?\//, options.showHighRes ? '/' : '/s800/');
+			_this.data('hoverZoomSrc', [src]).addClass('hoverZoomLink');
+			
+			var tooltip = _this.parent().find('.goog-icon-list-icon-meta:eq(0)');
+			if (tooltip.length) {
+				_this.data('hoverZoomCaption', tooltip.text());
+			}
+		}
+	
+		var links = $("a.goog-icon-list-icon-link,div.gphoto-grid-cell a");
+		links.live('mouseover', prepareImgLink);
+		callback(links);
+	}
+});
---
+++ src/tags/2.6/plugins/vk.js
@@ -0,0 +1,84 @@
+﻿// Copyright (c) 2011 Romain Vallet <romain.vallet@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'VK.com',
+	version: '0.1',
+	prepareImgLinks: function(callback) {
+
+		function prepareFromPhotoId(link, photoId, listId) {
+			if (!listId) {
+				listId = 'photos' + photoId.match(/(\d+)_/)[1];
+			}
+			chrome.extension.sendRequest({action: 'ajaxRequest', 
+				url: 'http://vk.com/al_photos.php',
+				method: 'POST',
+				data: 'al=1&act=show&photo=' + photoId + '&list=' + listId, 
+				headers: [{header: 'Content-Type', value: 'application/x-www-form-urlencoded'},
+				          {header: 'X-Requested-With', value: 'XMLHttpRequest'}]}, 
+				function(data) {
+					var photos;
+					try {
+						photos = JSON.parse(data.match(/<!json>(.*)<!>/)[1]);
+					} catch(e) {
+						return; 
+					}
+					for (var i in photos) {
+						if (photos[i].id == photoId) {
+							link.data('hoverZoomSrc', [photos[i].x_src]).addClass('hoverZoomLink');
+						} else {
+							// in case the request fetched details on another photo on the page
+							$('a[href^="/photo' + photos[i].id + '"]').data('hoverZoomSrc', [photos[i].x_src]).addClass('hoverZoomLink');
+						}
+					}
+					if (!link.data('hoverZoomMouseLeft')) {
+						hoverZoom.displayPicFromElement(link);
+					}
+				});
+		}
+	
+		$('a[href^="/photo"]').mouseenter(function () {
+			var link = $(this);
+			if (link.data('hoverZoomSrc')) { return; }
+			if (this.onclick) {
+				var onclick = this.onclick.toString();
+				if (onclick.indexOf('x_src:') > -1) {
+					link.data('hoverZoomSrc', [onclick.match(/x_src\s*:\s*"([^"]*)"/)[1]]).addClass('hoverZoomLink');
+				}
+			}
+			if (link.data('hoverZoomSrc') || link.data('hoverZoomRequested')) { return; }
+			link.data('hoverZoomRequested', true);
+			var listId, photoId = this.href.match(/\/photo(-?\d+_\d+).*/)[1];
+			if (this.href.indexOf('tag=') > -1) {
+				listId = 'tag' + this.href.match(/tag=(\d+)/)[1];
+			}
+			prepareFromPhotoId(link, photoId, listId);
+		}).mouseleave(function () {
+			$(this).data('hoverZoomMouseLeft', true);
+		});
+	
+		$('img[src*="/u"]').filter(function() {
+			return this.src.match(/\/u\d+\/[ed]_/);
+		}).mouseenter(function () {
+			var img = $(this);
+			if (img.data('hoverZoomRequested') || img.data('hoverZoomSrc')) { return; }
+			img.data('hoverZoomRequested', true);
+			var userId = this.src.match(/\/u(\d+)\//)[1];
+			chrome.extension.sendRequest({action: 'ajaxGet', url: 'http://vk.com/al_profile.php?al=1&act=get_profile_photos&offset=0&skip_one=0&id=' + userId}, function(data) {
+				var photos;
+				try {
+					photos = JSON.parse(data.match(/<!json>(.*)$/)[1]);
+				} catch(e) {
+					return; 
+				}
+				if (photos.length) {
+					prepareFromPhotoId(img, photos[0][1].match(/\/photo(\d+_\d+)/)[1], '');
+				}
+			});
+		}).mouseleave(function () {
+			$(this).data('hoverZoomMouseLeft', true);
+		});
+	
+	}
+});
