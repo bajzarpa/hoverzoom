9ebb0cc088f9a3d9727dd4950d2a30ce562ccb99
romain.vallet <romain.vallet@gmail.com>
2011-07-13 14:58:00 +0000

    Tagged version 3.0

---
+++ src/tags/3.0/plugins/googleplus.js
@@ -0,0 +1,43 @@
+﻿// Copyright (c) 2011 Romain Vallet <romain.vallet@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'Google+',
+	version: '0.1',
+	prepareImgLinks: function(callback) {
+		var res = [];
+		hoverZoom.urlReplace(res, 
+			'img:not(.a-b-c-z-pa):not(.a-Lc-Ct)[src*="?sz="]',
+			/\?sz=\d+/,
+			''
+		);		
+		hoverZoom.urlReplace(res, 
+			'img:not([src*="/news/tbn/"])[src*="proxy?url="]',
+			/.*proxy\?url=([^&]+).*/,
+			'$1'
+		);		
+		hoverZoom.urlReplace(res, 
+			'.ea-S img, .a-b-g-Mp-ba img',
+			/\/(w\d+-h\d+|[hws]\d+)(-[pck])*\//,
+			'/'
+		);		
+		// Circles thumbs
+		$('img.a-j-Db-z').each(function() {
+			var _this = $(this),
+				title = _this.siblings('.a-j-Db-Dg');
+			if (title.length > 0) {
+				_this.data().hoverZoomCaption = title.text();
+			}
+		});
+		// View photos of xxx with...
+		$('.a-b-g-Mp-ba img').each(function() {
+			var _this = $(this),
+				title = _this.parents('.ea-g-Vc').find('a.a-g-h');
+			if (title.length > 0) {
+				_this.data().hoverZoomCaption = title.text();
+			}
+		});
+		callback($(res));	
+	}
+});
---
+++ src/tags/3.0/plugins/twitpic_a.js
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2010 Romain Vallet
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+	name: 'Twitpic (a)',
+	version: '0.1',
+	prepareImgLinks: function(callback) {
+		var res = [];
+		hoverZoom.urlReplace(res, 
+			'a[href*="twitpic.com"]',
+			/twitpic\.com\/([^\/]*)$/,
+			'twitpic.com/show/' + (options.showHighRes ? 'full' : 'large') + '/$1'
+		);			
+		callback($(res));
+	}
+});
